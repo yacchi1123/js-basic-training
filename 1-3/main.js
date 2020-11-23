b7153babde3ae5a0e686d43d117798407ddea3fd
yamasakayuma <>
2020-11-24 00:37:05 +0900

    jsファイル修正

--- 1-3/main.js
+++ 1-3/main.js
@@ -2,7 +2,7 @@ const items = [
   { name: 'じゃがいも', price: 50},
   { name: 'にんじん', price: 60},
   { name: 'しいたけ', price: 200},
-  { name: '玉ねぎ', price: 50}
+  { name: '玉ねぎ', price: 100}
 ];
 
 const createItemList = (items) => {
